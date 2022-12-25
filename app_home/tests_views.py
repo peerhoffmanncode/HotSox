c9accc9cced6a19c55c7f27ce35f70962a95f8d5
PeerHoffmannCode <peerhoffmann81@gmail.com>
2022-12-25 15:18:40 +0100

    CHANGE: views.py in app_home to have it test ready ADD: test app_user.model.py ADD: test app_home.views.py

---
+++ app_home/tests_views.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from app_users.models import User
+from datetime import date
+
+
+class Test(TestCase):
+    def test_home_view_none_restricted(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "app_home/index.html")
+
+    def test_restricted_area_prove_of_concept_no_access(self):
+        # create test user
+        user = User.objects.create(
+            username="test",
+            first_name="test first",
+            last_name="test last",
+            email="test@mail.com",
+            password="str0ng_pwd!",
+            birthday=date(2000, 1, 1),
+            user_sex="female",
+            interested_sex="male",
+        )
+        response = self.client.get("/prove-of-concept/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.content,
+            b'<h1>Please first log in to access this page.</h1><br><a href="/user/login">Sign in</a>',
+        )
+
+    def test_restricted_area_prove_of_concept_access(self):
+        # create test user
+        user = User.objects.create(
+            username="test",
+            first_name="test first",
+            last_name="test last",
+            email="test@mail.com",
+            password="str0ng_pwd!",
+            birthday=date(2000, 1, 1),
+            user_sex="female",
+            interested_sex="male",
+        )
+        self.client.force_login(user=user)
+        response = self.client.get("/prove-of-concept/")
+        # request = response.request
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.content,
+            b'<h1>Seems like you are logged in! Yeay! </h1><p>Nice job Test!</p><br><a href="/">return to home</a>',
+        )
+
+    # # login(user)
