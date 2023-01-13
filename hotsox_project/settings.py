085eced534fd574036b7562abca05f718d62600b
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-01-13 19:43:48 +0100

    deactivated summernote

--- hotsox_project/settings.py
+++ hotsox_project/settings.py
@@ -57,14 +57,16 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "cloudinary",
     "crispy_forms",
-    "django_summernote",
+    # "django_summernote",
     "app_home",
     "app_users",
 ]
 
 # Social accounts login
 SOCIALACCOUNT_LOGIN_ON_GET = False
-AUTHENTICATION_BACKENDS = ["allauth.account.auth_backends.AuthenticationBackend"]
+AUTHENTICATION_BACKENDS = [
+    "allauth.account.auth_backends.AuthenticationBackend"
+]
 SOCIALACCOUNT_PROVIDERS = {
     "google": {
         "SCOPE": [
@@ -180,7 +182,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # initial work to use jwt token validation soon!
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
