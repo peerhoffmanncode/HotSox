790ec7c930f78e51d4d347ac12da9e4cc8b371c6
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-01-14 00:41:35 +0100

    pep8

--- hotsox_project/settings.py
+++ hotsox_project/settings.py
@@ -64,9 +64,7 @@ INSTALLED_APPS = [
 
 # Social accounts login
 SOCIALACCOUNT_LOGIN_ON_GET = False
-AUTHENTICATION_BACKENDS = [
-    "allauth.account.auth_backends.AuthenticationBackend"
-]
+AUTHENTICATION_BACKENDS = ["allauth.account.auth_backends.AuthenticationBackend"]
 SOCIALACCOUNT_PROVIDERS = {
     "google": {
         "SCOPE": [
@@ -183,9 +181,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # initial work to use jwt token validation soon!
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
