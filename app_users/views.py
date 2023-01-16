5bbfb73dc97bb99b8c2958cf5b02e314dd2d7fed
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-01-16 19:53:48 +0100

    feature: database redirected to remote elephant sql

--- app_users/views.py
+++ app_users/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
+from django.contrib.auth import login
 
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
