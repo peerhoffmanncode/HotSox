ed9c4d868cd1c09ba7bb788f4c0eb05789acb0b3
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-01-23 13:21:40 +0100

    added some security related aspects

--- hotsox_project/settings.py
+++ hotsox_project/settings.py
@@ -35,6 +35,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
