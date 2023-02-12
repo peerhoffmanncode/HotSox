4e9fb759456cb20aa86b121715bbf690bd09ec30
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-02-12 22:13:05 +0100

    redifined websocket url

--- app_chat/routing.py
+++ app_chat/routing.py
@@ -1,4 +1,4 @@
 from django.urls import re_path
 from . import consumers
 
-websocket_urlpatterns = [re_path(r"ws/chat/", consumers.ChatConsumer.as_asgi())]
+websocket_urlpatterns = [re_path(r"chat/", consumers.ChatConsumer.as_asgi())]
