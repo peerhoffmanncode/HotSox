459c7b95f73b5382aa1eef9fe10245086d768d40
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-02-15 23:56:35 +0100

    FEAT: you have a match page  ADD/UPDATE: profile details included map from folium

--- app_home/views.py
+++ app_home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 from django.db.models import Q
 import uuid
 
@@ -83,7 +84,7 @@ class SwipeView(HotSoxLogInAndValidationCheckMixin, TemplateView):
         # frontend liked the sock
         if request.POST.get("decision", None) == "like":
             # we use get_or_create to beware of duplicates!
-            _, sock_like_created = SockLike.objects.get_or_create(
+            tobedelete, sock_like_created = SockLike.objects.get_or_create(
                 sock=current_user_sock, like=sock_to_be_decided_on
             )
 
@@ -91,7 +92,7 @@ class SwipeView(HotSoxLogInAndValidationCheckMixin, TemplateView):
             if current_user_sock in sock_to_be_decided_on.get_likes():
                 # create match in UserMatchTable if not already exists!
                 try:
-                    user_match_created = UserMatch.objects.get(
+                    user_match_object = UserMatch.objects.get(
                         Q(user=current_user_sock.user, other=sock_to_be_decided_on.user)
                         | Q(
                             other=current_user_sock.user,
@@ -100,7 +101,7 @@ class SwipeView(HotSoxLogInAndValidationCheckMixin, TemplateView):
                     )
                     user_match_created = False
                 except UserMatch.DoesNotExist:
-                    user_match_created = UserMatch.objects.create(
+                    user_match_object = UserMatch.objects.create(
                         user=current_user_sock.user,
                         other=sock_to_be_decided_on.user,
                         chatroom_uuid=uuid.uuid4(),
@@ -108,11 +109,22 @@ class SwipeView(HotSoxLogInAndValidationCheckMixin, TemplateView):
                     user_match_created = True
 
                 if user_match_created:
-                    pass
                     # TODO:  create a modal dialog to inform the user about a match!
-                    #       > show short user details
                     #       send a info email
                     #       show some unicorn farts!
+                    context = {
+                        "user": current_user_sock.user,
+                        "user_sock": current_user_sock,
+                        "matched_user": sock_to_be_decided_on.user,
+                        "matched_user_sock": sock_to_be_decided_on,
+                    }
+                    # add navigation arrows
+                    context["left_arrow_go_to_url"] = reverse("app_home:swipe")
+                    context["right_arrow_go_to_url"] = reverse(
+                        "app_users:user-profile-details"
+                    )
+
+                    return render(request, "app_home/match.html", context)
 
         # frontend disliked the sock
         elif request.POST.get("decision", None) == "dislike":
