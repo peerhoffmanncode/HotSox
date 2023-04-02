0916fe3f3aeb34e4f04ec5d8f3a9cd15eadc4027
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-04-02 15:24:11 +0200

    updated get_geolocation_from_ip fucntion

--- django/app_users/forms.py
+++ django/app_users/forms.py
@@ -79,8 +79,8 @@ class UserSignUpForm(UserCreationForm):
                 GeoLocation.get_ip_address(self.request)
             )
         except:
-            city = ""
-        self.fields["location_city"].initial = city
+            city = {"city":""}
+        self.fields["location_city"].initial = city["city"]
 
     class Meta:
         model = User
