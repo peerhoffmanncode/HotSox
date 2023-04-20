ecd8ab49345a3d014e9366725a5cbbae7344af1b
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-04-20 13:47:32 +0200

    DJANGO reordering of model_choise attribute for prediction alogo the second time becuase of some fricken native speakers

--- django/app_users/models_choices.py
+++ django/app_users/models_choices.py
@@ -1,7 +1,7 @@
 GENDER_CHOICES = (
     ("1", "Female"),
     ("2", "Male"),
-    ("3", "Non-Binary"),
+    ("3", "Non-binary"),
     ("4", "One of a kind"),
     ("5", "Snail"),
     ("6", "Unicorn"),
@@ -10,12 +10,12 @@ GENDER_CHOICES = (
 )
 # CHOICES FOR SOCK
 COLOR_CHOICES = (
-    ("1", "Multicolor"),
-    ("2", "Black"),
-    ("3", "Grey"),
-    ("4", "Red"),
-    ("5", "Orange"),
-    ("6", "Yellow"),
+    ("1", "Black"),
+    ("2", "Grey"),
+    ("3", "Red"),
+    ("4", "Orange"),
+    ("5", "Yellow"),
+    ("6", "Multicolor"),
     ("7", "Blue"),
     ("8", "Purple"),
     ("9", "Green"),
@@ -23,22 +23,22 @@ COLOR_CHOICES = (
 )
 
 FABRIC_CHOICES = (
-    ("1", "Bamboo"),
-    ("2", "Wool"),
-    ("3", "Cotton"),
-    ("4", "Polyester"),
+    ("1", "Wool"),
+    ("2", "Cotton"),
+    ("3", "Polyester"),
+    ("4", "Bamboo"),
     ("5", "Nylon"),
     ("6", "Silk"),
     ("7", "Latex"),
 )
 
 FABRIC_THICKNESS_CHOICES = (
-    ("1", "Fishnets"),
+    ("1", "Fishnet"),
     ("2", "Nylon thin"),
     ("3", "Summer type"),
     ("4", "Regular polyester"),
     ("5", "Winter type"),
-    ("6", "Cushioned Cozy one"),
+    ("6", "Cushioned cozy one"),
     ("7", "Double battery powered self warming one"),
 )
 
@@ -50,9 +50,9 @@ BRAND_CHOICES = (
     ("5", "Adidas"),
     ("6", "Puma"),
     ("7", "Converse"),
-    ("8", "Odd socks"),
-    ("9", "Hot socks"),
-    ("10", "From'ma'grandma"),
+    ("8", "Odd sock"),
+    ("9", "Hot sock"),
+    ("10", "From ma' gran'ma"),
     ("11", "Aldi"),
     ("12", "One Euro Shop"),
     ("13", "Stolen"),
@@ -60,14 +60,14 @@ BRAND_CHOICES = (
 
 TYPE_CHOICES = (
     ("1", "Invisible"),
-    ("2", "Toe Socks"),
+    ("2", "Toe sock"),
     ("3", "Toe less"),
     ("4", "Ankle"),
-    ("5", "Crew"),
-    ("6", "Mid-Calf"),
+    ("5", "Mid-calf"),
+    ("6", "Crew"),
     ("7", "Calf"),
-    ("8", "Knee-High"),
-    ("9", "Ga'ma knitted socks"),
+    ("8", "Knee-high"),
+    ("9", "Gran'ma knitted sock"),
 )
 
 SIZE_CHOICES = (
@@ -82,27 +82,27 @@ SIZE_CHOICES = (
 
 CONDITION_CHOICES = (
     ("1", "Brand new"),
-    ("2", "Fairly used"),
-    ("3", "Sunday best"),
+    ("2", "Sunday best"),
+    ("3", "Fairly used"),
     ("4", "Well loved"),
-    ("5", "Regualar ones"),
+    ("5", "Regular one"),
     ("6", "Stained"),
     ("7", "Overused"),
     ("8", "Colorless but still going."),
     ("9", "Lost and found"),
-    ("10", "Involuntary Toesocks"),
+    ("10", "Involuntary Toesock"),
     ("11", "Rescued from trash"),
     ("12", "One of a kind"),
 )
 
 ENVIRONMENT_CHOICES = (
-    ("1", "Indoor Sock"),
-    ("2", "Netflix'n'Chill Sock"),
-    ("3", "Love-live Sock"),
-    ("4", "Sleeping Sock"),
-    ("5", "Las Vegas Sock"),
-    ("6", "Japanese vendor maschine Sock"),
-    ("7", "Gym Sock"),
-    ("8", "Outdoor Sock"),
-    ("9", "2-week used Festival Sock"),
+    ("1", "Indoor sock"),
+    ("2", "Netflix'n'Chill sock"),
+    ("3", "Love-live sock"),
+    ("4", "Sleeping sock"),
+    ("5", "Las Vegas sock"),
+    ("6", "Japanese vendor machine sock"),
+    ("7", "Gym sock"),
+    ("8", "Outdoor sock"),
+    ("9", "2-week used Festival sock"),
 )
