4973f863e0a47f7f91a1ec03d357c0d519796712
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-04-21 12:38:02 +0200

    DJANGO mail improvement

--- django/app_mail/tasks.py
+++ django/app_mail/tasks.py
@@ -6,7 +6,7 @@ from celery import shared_task
 
 @shared_task(name="send_email_in_background")
 def celery_send_mail(
-    email_subject, email_message: str, recipient_list: list, notification: bool
+    email_subject: str, email_message: str, recipient_list: list, notification: bool
 ):
     if notification == True:
         return send_mail(
