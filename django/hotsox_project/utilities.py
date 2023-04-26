a2dbf3ca1bae34d7df8c7533e75f92b9c472fa92
dragon-fire-fly <ek93@hotmail.co.uk>
2023-04-26 21:12:18 +0200

    FEAT: Add cookie notification banner

--- django/hotsox_project/utilities.py
+++ django/hotsox_project/utilities.py
@@ -1,5 +1,7 @@
 from allauth.socialaccount.models import SocialApp
 from django.contrib.sites.models import Site
+from app_users.models import User
+import datetime
 
 
 def create_db_entry_social_app(
@@ -15,7 +17,8 @@ def create_db_entry_social_app(
         site = Site.objects.create(domain=site_domain, name=site_name)
 
     try:
-        # Check if a social application with the given provider name already exists
+        # Check if a social application with the given provider name
+        # already exists
         social_app = SocialApp.objects.get(provider=provider)
         if social_app.client_id != client_id or social_app.secret != secret:
             social_app.client_id = client_id
@@ -29,6 +32,19 @@ def create_db_entry_social_app(
         )
         social_app.sites.add(site)
         social_app.save()
+
+    # Checks if there are any users in the database. If not, uses env file
+    # details to create a superuser (so admin panel can be accessed)
+    if len(User.objects.all()) == 0 and os.environ.get("ADMIN_USERNAME"):
+        User.objects.create_superuser(
+            username=os.environ.get("ADMIN_USERNAME", None),
+            password=os.environ.get("ADMIN_PWD", None),
+            email="admin@admin.com",
+            info_birthday=datetime.date(2000, 1, 1),
+            info_gender="1",
+            location_city="Berlin",
+            notification=True,
+        )
 
     # return current SITE_ID
     return created, Site.objects.get(domain=site_domain).id
